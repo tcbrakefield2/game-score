9c42d8c73dc87fedac72434a8629eb27f05f93cb
tcbrakefield2 <253540456+tcbrakefield2@users.noreply.github.com>
2026-04-01 09:54:51 -0500

    Change default player name from 'Garfield' to 'Thomas'

--- scripts/defaults.js
+++ scripts/defaults.js
@@ -1,10 +1,10 @@
-export const DEFAULT_PLAYER_NAME = 'Garfield';
+export const DEFAULT_PLAYER_NAME = 'Thomas';
 
 export const DEFAULT_LEADERBOARD = [
-  { name: 'Garfield', score: 9000 },
+  { name: 'Thomas', score: 9000 },
   { name: 'Nermal', score: 7000 },
   { name: 'Odie', score: 5000 },
   { name: 'Arlene', score: 3000 }
 ];
 
-export const GARFIELD_NEW_SCORE = 9500;
+export const THOMAS_NEW_SCORE = 9500;
